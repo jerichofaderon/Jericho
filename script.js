1c09ff1347d50f0ee8a2499a8160f03780869b77
Jericho <jerichofaderon@gmail.com>
2026-03-11 11:00:17 -0400

    Remove header shadow effect on scroll
    Removed scroll event listener for header shadow effect.

--- script.js
+++ script.js
@@ -3,16 +3,3 @@ document.getElementById(id).scrollIntoView({
 behavior:"smooth"
 });
 }
-
-window.addEventListener("scroll", () => {
-
-const header = document.querySelector("header");
-
-if(window.scrollY > 50){
-header.style.boxShadow = "0 5px 20px rgba(0,0,0,0.3)";
-}
-else{
-header.style.boxShadow = "none";
-}
-
-});
